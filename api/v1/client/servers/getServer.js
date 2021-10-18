d795222fdd6c86c233018655010ff503bd941989
Joseph Maldjian <josephavm@icloud.com>
2021-10-17 22:33:49 -0500

    Add Server ID validation to getServer

--- api/v1/client/servers/getServer.js
+++ api/v1/client/servers/getServer.js
@@ -3,6 +3,7 @@ const { ObjectId } = require("mongodb");
 const client = require("../../../../lib/mongodb");
 
 async function getServer(req, res) {
+  if (typeof(req.params.magma_cube) != "string" || Buffer.byteLength(req.params.magma_cube, 'utf8') < 12) return res.send({status: "error", data: "Invalid Server ID"});
   const collection = client
     .db(`${process.env.DATABASE_NAME}`)
     .collection("servers");
