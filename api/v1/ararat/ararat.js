943efc1d233ec351939833dd92d686d91f8f22da
Joseph Maldjian <josephavm@icloud.com>
2021-10-18 10:45:16 -0500

    Remove Ararat Routes
    Uneccessarry

--- api/v1/ararat/ararat.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authenticationAPI = require("./authentication/authenticationAPI");
-
-
-module.exports = router;
--- api/v1/v1.js
+++ api/v1/v1.js
@@ -6,5 +6,4 @@ const admin = require("./admin/admin");
 
 router.use("/client", client);
 router.use("/admin", admin);
-if (process.env.Ararat == true) router.use("/ararat", ararat);
 module.exports = router;
