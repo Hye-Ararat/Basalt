d367c94d7e2bf2c68489b1f89bb06c5a4c4d7ff9
Joseph Maldjian <josephavm@icloud.com>
2021-10-18 11:45:23 -0500

    Add auth routes

--- api/v1/client/client.js
+++ api/v1/client/client.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const authAPI = require("./auth/authAPI");
 const serverAPI = require("./servers/serverAPI");
+
+//Auth Routes
+router.post("/auth/login", authAPI.login);
+router.post("/auth/create", authAPI.create);
 
 //Server Routes
 router.get("/servers/:server", serverAPI.getServer);
