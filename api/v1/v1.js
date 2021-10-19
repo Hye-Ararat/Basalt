f5acaf21c12b3148d720df46e52eeb5ede12d648
Joseph Maldjian <josephavm@icloud.com>
2021-10-19 12:04:32 -0500

    Add Authorization to Client Endpoints

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 const client = require("./client/client");
 const admin = require("./admin/admin");
 
-router.use("/client", client);
+router.use("/client", function(req, res, next) {
+    if (req.path == "/auth/login") return next();
+    if (!req.headers["authorization"]) return res.json({status: "error", data: "No authorization header sent"});
+    const authorization = req.headers["authorization"].split(" ")[1];
+    if (authorization.substring(0, authorization.indexOf(":"))) {
+        //Is API key
+    } else {
+        try {
+            jwt.verify(authorization, process.env.ACCESS_TOKEN_SECRET)
+        } catch (error) {
+            return res.json({status: "error", data: "Authorization is incorrect"});
+        }
+        return next();
+    }
+}, client);
 router.use("/admin", admin);
 module.exports = router;
