974ef90ad9b9a4a4e4c949f1fca115d931620a47
Joseph Maldjian <josephavm@icloud.com>
2021-10-20 15:25:15 -0500

    Add Rate Limiting

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const fs = require("fs");
+const rateLimiter = require("express-rate-limit");
 const router = express.Router();
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+
+const rateLimit = rateLimiter({
+	windowMs: 60000,
+	max: 100,
+});
+
+app.use(rateLimit);
+app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 const apiv1 = require("./api/v1/v1");
@@ -14,33 +22,33 @@ app.use(router);
 router.use("/api/v1", apiv1);
 
 try {
-  var key = fs.readFileSync(process.env.SSL_KEY);
+	var key = fs.readFileSync(process.env.SSL_KEY);
 } catch (error) {
-  console.log("Error while reading SSL Key")
-  process.exit(0);
+	console.log("Error while reading SSL Key");
+	process.exit(0);
 }
 try {
-  var cert = fs.readFileSync(process.env.SSL_CERT);
+	var cert = fs.readFileSync(process.env.SSL_CERT);
 } catch (error) {
-  console.log("Error while reading SSL Cert");
-  process.exit(0);
+	console.log("Error while reading SSL Cert");
+	process.exit(0);
 }
 let httpsServer = https.createServer(
-  {
-    key: key,
-    cert: cert,
-  },
-  app
+	{
+		key: key,
+		cert: cert,
+	},
+	app
 );
 httpsServer.on("error", (e) => {
-  console.log(e.message);
+	console.log(e.message);
 });
 
 httpsServer.listen(process.env.LISTEN_PORT, process.env.LISTEN_ADDRESS);
 
 httpsServer.on("listening", () => {
-  console.log(
-    "Server Running on",
-    process.env.LISTEN_ADDRESS + ":" + process.env.LISTEN_PORT
-  );
+	console.log(
+		"Server Running on",
+		process.env.LISTEN_ADDRESS + ":" + process.env.LISTEN_PORT
+	);
 });
