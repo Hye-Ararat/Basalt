20990a8448296cfdc4d0d6d158b50851d0355f16
Joseph Maldjian <josephavm@icloud.com>
2021-10-25 09:29:41 -0500

    Check if user has access to server

--- api/v1/client/servers/getServer.js
+++ api/v1/client/servers/getServer.js
@@ -3,21 +3,21 @@ const { ObjectId } = require("mongodb");
 const client = require("../../../../lib/mongodb");
 
 async function getServer(req, res) {
-  if (
-    typeof req.params.server != "string" ||
-    Buffer.byteLength(req.params.server, "utf8") < 12
-  )
-    return res.send({ status: "error", data: "Invalid Server ID" });
-  const collection = client
-    .db(`${process.env.DATABASE_NAME}`)
-    .collection("servers");
-  const server_data = await collection.findOne({
-    _id: ObjectId(req.params.server),
-    [`users.${res.locals.user_data.id}`]: { $exists: true },
-  });
-  server_data == null
-    ? res.json({ status: "error", data: "Server does not exist" })
-    : res.json({ status: "success", data: server_data });
+	if (
+		typeof req.params.server != "string" ||
+		Buffer.byteLength(req.params.server, "utf8") < 12
+	)
+		return res.send({ status: "error", data: "Invalid Server ID" });
+	const collection = client
+		.db(`${process.env.DATABASE_NAME}`)
+		.collection("servers");
+	const server_data = await collection.findOne({
+		_id: ObjectId(req.params.server),
+		[`users.${res.locals.user_data.id}`]: { $exists: true },
+	});
+	server_data == null
+		? res.json({ status: "error", data: "Server does not exist" })
+		: res.json({ status: "success", data: server_data });
 }
 
 module.exports = getServer;
