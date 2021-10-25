108c2bd068de09a102c075f9649ad6d2bbbeefcc
Joseph Maldjian <josephavm@icloud.com>
2021-10-25 09:30:46 -0500

    Switch from hardcoded to automatic on server listing

--- api/v1/client/getServers.js
+++ api/v1/client/getServers.js
@@ -3,14 +3,13 @@ const { ObjectId } = require("mongodb");
 const client = require("../../../lib/mongodb");
 
 async function getServers(req, res) {
-	var user = "616da13fe2f36f19e274a7ca";
 	const collection = client
 		.db(`${process.env.DATABASE_NAME}`)
 		.collection("servers");
 	try {
 		var server_data = await collection
 			.find({
-				[`users.${user}`]: { $exists: true },
+				[`users.${res.locals.user_data.id}`]: { $exists: true },
 			})
 			.toArray();
 	} catch (error) {
