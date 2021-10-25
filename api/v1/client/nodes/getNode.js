49d016d18ad11e06b25cad5badaba48c37a5f84d
Joseph Maldjian <josephavm@icloud.com>
2021-10-25 09:28:19 -0500

    Check if user has access to node information

--- api/v1/client/nodes/getNode.js
+++ api/v1/client/nodes/getNode.js
@@ -3,20 +3,29 @@ const { ObjectId } = require("mongodb");
 const client = require("../../../../lib/mongodb");
 
 async function getNode(req, res) {
-  if (
-    typeof req.params.node != "string" ||
-    Buffer.byteLength(req.params.node, "utf8") < 12
-  )
-    return res.send({ status: "error", data: "Invalid Node ID" });
-  const collection = client
-    .db(`${process.env.DATABASE_NAME}`)
-    .collection("nodes");
-  const node_data = await collection.findOne({
-    _id: ObjectId(req.params.node),
-  });
-  node_data == null
-    ? res.json({ status: "error", data: "Node does not exist" })
-    : res.json({ status: "success", data: node_data });
+	if (
+		typeof req.params.node != "string" ||
+		Buffer.byteLength(req.params.node, "utf8") < 12
+	)
+		return res.send({ status: "error", data: "Invalid Node ID" });
+	const server_collection = client
+		.db(`${process.env.DATABASE_NAME}`)
+		.collection("servers");
+	const server = await server_collection.findOne({
+		node: req.params.node,
+		[`users.${req.locals.user_data.id}`]: { $exists: true },
+	});
+	if (server == null)
+		return res.send({ status: "error", data: "Node not found" });
+	const collection = client
+		.db(`${process.env.DATABASE_NAME}`)
+		.collection("nodes");
+	const node_data = await collection.findOne({
+		_id: ObjectId(req.params.node),
+	});
+	node_data == null
+		? res.json({ status: "error", data: "Node does not exist" })
+		: res.json({ status: "success", data: node_data });
 }
 
 module.exports = getNode;
